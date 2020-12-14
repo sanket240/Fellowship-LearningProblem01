b092f730c0f38dc0893f034221527be71a9044e8
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 21:06:11 +0530

    Added : Daily employee wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,13 +1,18 @@
 public class EmpWageBuilder
 {
+        static final int IS_FULL_TIME=1;
+        static final int EMP_RATE_PER_HOUR=20;
         public static void main(String[] args)
         {
-                int IS_FULL_TIME=1;
+		int empHrs=0;
+		int empWage=0;
                 double empCheck=Math.floor(Math.random()*10)%2;
                 if(empCheck==IS_FULL_TIME)
-                        System.out.println("Employee is Present");
+                        empHrs=8;
                 else
-                        System.out.println("Employee is Absent");
+                        empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
         }
 }
 
