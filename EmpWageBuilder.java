1bff87c679a4e3911acd7cf2c532a2051883a2dd
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 21:51:31 +0530

    Added : Conditional Wages

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,12 +4,13 @@ public class EmpWageBuilder
 	public static final int IS_PART_TIME=1;
         public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
         public static void main(String[] args)
         {
-		int empHrs=0;
-		int empWage=0,totalEmpWage=0;
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		int empWage=0,totalEmpWage=0,empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
+			totalWorkingDays++;
                 	double empCheck=Math.floor(Math.random()*10)%3;
 			switch((int)empCheck)
 			{
@@ -23,6 +24,7 @@ public class EmpWageBuilder
 					empHrs=0;
 					break;
                 	}
+			totalEmpHrs+=empHrs;
 			empWage=empHrs*EMP_RATE_PER_HOUR;
 			totalEmpWage+=empWage;
 		}
